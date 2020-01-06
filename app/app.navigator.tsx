7974706c0dc86473a7dcf3c388d4a0d8182a7be3
Artak-Mkrtchyan <30382535+Artak-Mkrtchyan@users.noreply.github.com>
2020-01-06 13:08:15 +0400

    feat(firebase): added uploadImage to firebase function

--- app/app.navigator.tsx
+++ app/app.navigator.tsx
@@ -1,22 +1,22 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-import LoginScreen from './screens/LoginScreen/Login.screen';
-import RegistrationScreen from './screens/RegistrationScreen/Registration.screen';
-import HomeScreen from './screens/HomeScreen/Home.screen';
+import LoginScreen from "./screens/LoginScreen/Login.screen";
+import RegistrationScreen from "./screens/RegistrationScreen/Registration.screen";
+import HomeScreen from "./screens/HomeScreen/Home.screen";
 
 const MainNavigator = createStackNavigator(
-  {
-    Login: {screen: LoginScreen, navigationOptions: {title: 'Login'}},
-    Home: {screen: HomeScreen, navigationOptions: {title: 'Home'}},
-    Registration: {
-      screen: RegistrationScreen,
-      navigationOptions: {title: 'Registration'},
-    },
-  },
-  {
-    initialRouteName: 'Home',
-  },
+	{
+		Login: { screen: LoginScreen, navigationOptions: { title: "Login" } },
+		Home: { screen: HomeScreen, navigationOptions: { title: "Home" } },
+		Registration: {
+			screen: RegistrationScreen,
+			navigationOptions: { title: "Registration" }
+		}
+	},
+	{
+		initialRouteName: "Login"
+	}
 );
 
 const AppNavigator = createAppContainer(MainNavigator);
--- app/components/HomeComponent/Home.component.tsx
+++ app/components/HomeComponent/Home.component.tsx
@@ -1,85 +1,84 @@
-import React, {useState} from 'react';
-import {
-  Text,
-  View,
-  StyleSheet,
-  Image,
-  SafeAreaView,
-  Button,
-} from 'react-native';
-import ImagePicker from 'react-native-image-picker';
-import * as firebase from 'firebase/app';
-import 'firebase/storage';
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Image, SafeAreaView, Button } from "react-native";
+import ImagePicker from "react-native-image-picker";
+import * as firebase from "firebase/app";
+import "firebase/storage";
 
-import {IProps} from './types';
+import { IProps } from "./types";
 
 const options = {
-  title: 'Select Image',
-  storageOptions: {
-    skipBackup: true,
-    path: 'images',
-    cameraRoll: true,
-    waitUntilSaved: true,
-  },
+	title: "Select Image",
+	storageOptions: {
+		skipBackup: true,
+		path: "images",
+		cameraRoll: true,
+		waitUntilSaved: true
+	}
 };
 
 const Home: React.FC<IProps> = props => {
-  const [source, setSource] = useState({});
+	const [source, setSource] = useState({});
 
-  function pickImage() {
-    ImagePicker.showImagePicker(options, response => {
-      console.log('Response = ', response);
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      } else {
-        console.log('else');
-        const source = {uri: response.uri};
-        // You can also display the image using data:
-        // const source = {uri: 'data:image/jpeg;base64,' + response.data};
-        setSource(source);
-        // uploadImage(response.uri);
-        // const blob = uriToBlob(response.uri);
-        // uploadToFirebase(blob);
-      }
-    });
-  }
+	const uploadImage = async uri => {
+		const response = await fetch(uri);
+		const blob = await response.blob();
+		var ref = firebase
+			.storage()
+			.ref("uploads")
+			.child("file.jpg");
+		return ref.put(blob);
+	};
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        {source && source.uri ? (
-          <Image source={source} style={styles.image} />
-        ) : (
-          <Text>Select an Image!</Text>
-        )}
-        <View style={{height: 100}}>
-          <Button title="Pick image" onPress={() => pickImage()} />
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+	function pickImage() {
+		ImagePicker.showImagePicker(options, response => {
+			// console.log("Response = ", response);
+			if (response.didCancel) {
+				console.log("User cancelled image picker");
+			} else if (response.error) {
+				console.log("ImagePicker Error: ", response.error);
+			} else if (response.customButton) {
+				console.log("User tapped custom button: ", response.customButton);
+			} else {
+				console.log("else");
+				const source = { uri: response.uri };
+				setSource(source);
+				uploadImage(response.uri).then(res => console.log("upload"));
+			}
+		});
+	}
+
+	return (
+		<SafeAreaView style={styles.container}>
+			<View>
+				{source && source.uri ? (
+					<Image source={source} style={styles.image} />
+				) : (
+					<Text>Select an Image!</Text>
+				)}
+				<View style={{ height: 100 }}>
+					<Button title='Pick image' onPress={() => pickImage()} />
+				</View>
+			</View>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    flex: 1,
-    marginTop: 10,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    backgroundColor: '#ecf0f1',
-  },
-  image: {
-    marginTop: 20,
-    marginBottom: 20,
-    width: 150,
-    height: 150,
-  },
+	container: {
+		display: "flex",
+		flexDirection: "column",
+		flex: 1,
+		marginTop: 10,
+		alignItems: "center",
+		justifyContent: "space-between",
+		backgroundColor: "#ecf0f1"
+	},
+	image: {
+		marginTop: 20,
+		marginBottom: 20,
+		width: 150,
+		height: 150
+	}
 });
 
 export default Home;
