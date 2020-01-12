fb8180297a228a2d881acc37a664d40f62a6a1d5
Artak-Mkrtchyan <30382535+Artak-Mkrtchyan@users.noreply.github.com>
2020-01-13 01:27:50 +0400

    del

--- app/firebase/index.ts
+++
@@ -1,7 +0,0 @@
-import FirebaseContext, {withFirebase} from './context';
-import Firebase from './firebase';
-import firebaseConfig from './firebaseConfig';
-
-export default Firebase;
-
-export {FirebaseContext, withFirebase, firebaseConfig};
--- app/screens/GlobalStyles.ts
+++
@@ -1,9 +0,0 @@
-import { StyleSheet, Platform, StatusBar } from "react-native";
-
-export default StyleSheet.create({
-	AndroidSafeArea: {
-		flex: 1,
-		backgroundColor: "#ecf0f1",
-		paddingTop: Platform.OS === "android" ? 25 : 0
-	}
-});
